4f8ca348d6e678d84c492094b8b5470ce48b069f
Brent Shaffer <betterbrent@google.com>
2017-10-27 11:48:05 -0700

    adds bigquery sync query sample

---
+++ bigquery/mix.exs
@@ -0,0 +1,28 @@
+defmodule GoogleApi.BigQuery.Samples.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+      app: :bigquery_samples,
+      version: "0.0.1",
+      elixir: "~> 1.5",
+      start_permanent: Mix.env == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger, :goth]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:google_api_big_query , "~> 0.0.1"},
+      {:goth, "~> 0.6.0"}
+    ]
+  end
+end
