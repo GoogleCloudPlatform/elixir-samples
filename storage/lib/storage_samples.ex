e43ad70de205f65d689b0d867ab4dbc29a62bbc8
Brent Shaffer <betterbrent@google.com>
2017-10-27 13:49:27 -0700

    add storage upload

--- storage/lib/storage_samples.ex
+++ storage/lib/storage_samples.ex
@@ -18,6 +18,35 @@ defmodule GoogleApi.Storage.Samples do
     conn = GoogleApi.Storage.V1.Connection.new(token.token)
     {:ok, response} = GoogleApi.Storage.V1.Api.Buckets.storage_buckets_list(conn, project_id)
     Enum.each(response.items, &IO.puts(&1.id))
-    "done!"
   end
+
+  # [start storage_upload_file]
+  @doc """
+  List storage buckets for a project.
+
+  ## Examples
+
+      iex> GoogleApi.Storage.Samples.upload_file("bucket_id", "test/file.txt")
+      Uploaded file.text to  https://www.googleapis.com/storage/v1/b/bucket_id/o/file.txt
+      your_project_bucket2
+
+  """
+  def upload_file(bucket_id, file_path) do
+    # Authenticate.
+    {:ok, token} = Goth.Token.for_scope("https://www.googleapis.com/auth/cloud-platform")
+    conn = GoogleApi.Storage.V1.Connection.new(token.token)
+
+    # Make the API request.
+    {:ok, object} = GoogleApi.Storage.V1.Api.Objects.storage_objects_insert_simple(
+      conn,
+      bucket_id,
+      "multipart",
+      %{name: Path.basename(file_path)},
+      file_path
+    )
+
+    # Print the object.
+    IO.puts("Uploaded #{object.name} to #{object.selfLink}")
+  end
+  # [end storage_upload_file]
 end
--- storage/test/storage_samples_test.exs
+++ storage/test/storage_samples_test.exs
@@ -1,14 +1,23 @@
 defmodule GoogleApi.Storage.Samples.Test do
   use ExUnit.Case
   import ExUnit.CaptureIO
-  doctest GoogleApi.Storage.Samples, except: [:moduledoc, list_buckets: 1]
 
   test "lists buckets" do
     project_id = System.get_env("GOOGLE_PROJECT_ID")
     assert project_id
     output = capture_io(fn ->
-        GoogleApi.Storage.Samples.list_buckets(project_id)
+      GoogleApi.Storage.Samples.list_buckets(project_id)
     end)
     assert String.contains? output, project_id
+  end
+
+  test "upload object" do
+    # Use the project ID as the bucket ID
+    bucket_id = System.get_env("GOOGLE_PROJECT_ID")
+    assert bucket_id
+    output = capture_io(fn ->
+      GoogleApi.Storage.Samples.upload_file(bucket_id, "test/data/file.txt")
+    end)
+    assert String.contains? output, "Uploaded file.txt to https://www.googleapis.com/storage/v1/b/#{bucket_id}/o/file.txt"
   end
 end
