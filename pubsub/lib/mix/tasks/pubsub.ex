502ca4a660cbe8b8efcc2b95798332b8bb0f4c47
Brent Shaffer <betterbrent@google.com>
2017-11-07 13:53:35 -0800

    Style changes for pubsub

--- pubsub/lib/mix/tasks/pubsub.ex
+++ pubsub/lib/mix/tasks/pubsub.ex
@@ -5,58 +5,66 @@ defmodule Mix.Tasks.Pubsub do
   def run(_) do
     {:ok, _started} = Application.ensure_all_started(:goth)
     :httpc.set_options(pipeline_timeout: 1000)
-
-    project_id = (IO.gets "Project ID?: ")
-        |> String.trim("\n")
-    topic_name = (IO.gets "Topic Name?: ")
-        |> String.trim("\n")
-
+    project_id =
+      "Project ID?: "
+      |> IO.gets()
+      |> String.trim("\n")
+    topic_name =
+      "Topic Name?: "
+      |> IO.gets()
+      |> String.trim("\n")
     topic = get_or_create_topic(project_id, topic_name)
-
     if topic do
-        subscription_name = (IO.gets "Subscription Name?: ")
-            |> String.trim("\n")
-        subscription = get_or_create_subscription(project_id, topic_name, subscription_name)
-
-        if subscription do
-            {:ok, pid} = GoogleApi.PubSub.Samples.start_subscription_supervisor(project_id, subscription_name)
-            get_next_message(project_id, topic_name)
-        end
+      subscription_name =
+        "Subscription Name?: "
+        |> IO.gets()
+        |> String.trim("\n")
+      subscription = get_or_create_subscription(project_id, topic_name, subscription_name)
+      if subscription do
+        {:ok, _pid} = GoogleApi.PubSub.Samples.start_subscription_supervisor(project_id, subscription_name)
+        get_next_message(project_id, topic_name)
+      end
     end
   end
 
   defp get_or_create_topic(project_id, topic_name) do
-    topic = GoogleApi.PubSub.Samples.get_topic(project_id, topic_name)
-    if topic == nil do
-        response = (IO.gets "Topic does not exist. Create it? (y/n): ")
-            |> String.trim("\n")
-        if response == "y" do
-            GoogleApi.PubSub.Samples.create_topic(project_id, topic_name)
-        end
-    else
-        topic
+    GoogleApi.PubSub.Samples.get_topic(project_id, topic_name) ||
+      (confirm_topic_creation() && GoogleApi.PubSub.Samples.create_topic(project_id, topic_name)
+  end
+
+  defp confirm_topic_creation do
+    "Topic does not exist. Create it? (y/n): "
+    |> IO.gets()
+    |> String.trim("\n")
+    |> case do
+      "y" -> true
+      _ -> nil
     end
   end
 
   defp get_or_create_subscription(project_id, topic_name, subscription_name) do
-    subscription = GoogleApi.PubSub.Samples.get_subscription(project_id, subscription_name)
-    if subscription == nil do
-        response = (IO.gets "Subscription does not exist. Create it? (y/n): ")
-            |> String.trim("\n")
-        if response == "y" do
-            GoogleApi.PubSub.Samples.create_subscription(project_id, topic_name, subscription_name)
-        end
-    else
-        subscription
+    GoogleApi.PubSub.Samples.get_subscription(project_id, subscription_name) ||
+      (confirm_subscription_creation() && GoogleApi.PubSub.Samples.create_subscription(project_id, topic_name, subscription_name)
+  end
+
+  defp confirm_subscription_creation do
+    "Subscription does not exist. Create it? (y/n): "
+    |> IO.gets()
+    |> String.trim("\n")
+    |> case do
+      "y" -> true
+      _ -> nil
     end
   end
 
   defp get_next_message(project_id, topic_name) do
-    message = (IO.gets "Enter a pubsub message, or (q) to quit: ")
-        |> String.trim("\n")
+    message =
+      "Enter a pubsub message, or (q) to quit: "
+      |> IO.gets()
+      |> String.trim("\n")
     if !Enum.member?(["q", ""], message) do
-        GoogleApi.PubSub.Samples.publish(project_id, topic_name, message)
-        get_next_message(project_id, topic_name)
+      GoogleApi.PubSub.Samples.publish(project_id, topic_name, message)
+      get_next_message(project_id, topic_name)
     end
   end
 end
