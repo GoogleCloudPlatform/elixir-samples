66f6058b51e20b7f938d339e1f29b93ba77ffb8e
Brent Shaffer <betterbrent@google.com>
2017-11-07 13:36:15 -0800

    Update bigquery_samples_test.exs

--- bigquery/test/bigquery_samples_test.exs
+++ bigquery/test/bigquery_samples_test.exs
@@ -6,9 +6,9 @@ defmodule GoogleApi.BigQuery.Samples.Test do
     project_id = System.get_env("GOOGLE_PROJECT_ID")
     assert project_id
     output = capture_io(fn ->
-        sql = "SELECT TOP(corpus, 10) as title, COUNT(*) as unique_words
-                FROM [publicdata:samples.shakespeare]"
-        GoogleApi.BigQuery.Samples.sync_query(project_id, sql)
+      sql = "SELECT TOP(corpus, 10) as title, COUNT(*) as unique_words
+             FROM [publicdata:samples.shakespeare]"
+      GoogleApi.BigQuery.Samples.sync_query(project_id, sql)
     end)
     assert String.contains? output, "hamlet"
   end
