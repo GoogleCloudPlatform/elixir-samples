1aba612cdd11c9610a0d9083dcd294ae30083a82
Brent Shaffer <betterbrent@google.com>
2018-02-26 11:13:14 -0800

    delete some unneeded files

--- auth/web/models/user.ex
+++
@@ -1,9 +0,0 @@
-defmodule GoogleOAuth2Example.User do
-  @moduledoc """
-  The `current_user` is stored in the connection as a map
-  with string keys.
-  """
-
-  def name(user), do: user["name"]
-  def email(user), do: user["email"]
-end
